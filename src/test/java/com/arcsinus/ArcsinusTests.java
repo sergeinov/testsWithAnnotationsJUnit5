4992f7e142f3b9ad83e00c7918672e944b7441b0
tester <lookbugs@mail.ru>
2021-09-22 21:21:44 +0300

    added couple of tests

--- src/test/java/com/arcsinus/ArcsinusTests.java
+++ src/test/java/com/arcsinus/ArcsinusTests.java
@@ -2,8 +2,16 @@ package com.arcsinus;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class ArcsinusTests {
+
+    static final String URL = "https://arcsinus.ru/";
 
     @BeforeAll
     static void setup() {
@@ -11,12 +19,38 @@ public class ArcsinusTests {
         Configuration.startMaximized = true;
     }
 
-    void checkMenuButton(){
-
+    @Test
+    void checkMenuButton() {
+        // Step 1: Go to the site
+        open(URL);
+        // Step 2: Click on the button to open menu
+        $(".header--nav--btn").click();
+        // Step 3: Close the window of menu
+        $("#close-nav").click();
     }
 
+    @Test
     void checkNavMenu() {
-
+        // Step 1: Go to the site
+        open(URL);
+        // Step 2: Click on the link "Компания"
+        $(".header--nav--btn").click();
+        $(linkText("Компания")).click();
+        // Step 3: Click on the link "Проекты"
+        $(".header--nav--btn").click();
+        $(linkText("Проекты")).click();
+        // Step 4: Click on the link "Клиенты"
+        $(".header--nav--btn").click();
+        $(linkText("Клиенты")).click();
+        // Step 5: Click on the link "Команда"
+        $(".header--nav--btn").click();
+        $(linkText("Команда")).click();
+        // Step 6: Click on the link "Отзывы"
+        $(".header--nav--btn").click();
+        $(linkText("Отзывы")).click();
+        // Step 7: Click on the link "Контакты"
+        $(".header--nav--btn").click();
+        $(linkText("Контакты")).click();
     }
 
 }
